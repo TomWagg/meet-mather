49edf4d7029b1fb9fdfb946cf48f9119a4dd0ce1
TomWagg <twagg@college.harvard.edu>
2019-08-01 13:44:50 -0400

    fix css urls

---
+++ static/js/profile.js
@@ -0,0 +1,26 @@
+$(function() {
+    document.querySelector("select[name='concentration']").value = document.getElementById("prev_conc").value;
+    update_preview();
+    $(".preview-link").on("keyup change", function() {
+        update_preview();
+    });
+});
+
+/**
+ * Update the preview of your profile
+ */
+function update_preview() {
+    const card = document.querySelector(".preview .card");
+    card.querySelectorAll(".profile-first-name").forEach((el) => {
+        el.innerHTML = document.querySelector("input[name='name_first']").value;
+    });
+    card.querySelectorAll(".profile-last-name").forEach((el) => {
+        el.innerHTML = document.querySelector("input[name='name_last']").value;
+    });
+    card.querySelectorAll(".profile-year").forEach((el) => {
+        el.innerHTML = document.querySelector("input[name='year']").value;
+    });
+    card.querySelectorAll(".profile-concentration").forEach((el) => {
+        el.innerHTML = document.querySelector("select[name='concentration']").value;
+    });
+}
